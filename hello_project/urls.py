61d3b06300ad9c43a8b6fa785678085a445f3a75
anuar <xudaibergenov1973@gmail.com>
2026-02-28 11:53:23 +0500

    Fix urls.py syntax error

--- hello_project/urls.py
+++ hello_project/urls.py
@@ -8,5 +8,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),  # все эндпоинты будут начинаться с /api/
     path('api/login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('', TemplateView.as_view(template_name='index.html'))
+    path('', TemplateView.as_view(template_name='index.html')),
 ]
