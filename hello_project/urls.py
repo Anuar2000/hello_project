576f054888d96a539148f422a3e75553af83a754
anuar <xudaibergenov1973@gmail.com>
2026-02-28 11:30:40 +0500

    Add root path for home page

--- hello_project/urls.py
+++ hello_project/urls.py
@@ -1,10 +1,12 @@
 from django.contrib import admin
 from django.urls import path, include
 from django.urls import path
+from django.views.generic import TemplateView
 from rest_framework_simplejwt.views import TokenObtainPairView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),  # все эндпоинты будут начинаться с /api/
     path('api/login/', TokenObtainPairView.as_view(), name='token_obtain_pair')
+    path('', TemplateView.as_view(template_name='index.html'))
 ]
