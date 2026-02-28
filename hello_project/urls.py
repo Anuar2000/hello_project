bcedd5c963762513ccdea5f8ccdc16dbd79fdedc
anuar <xudaibergenov1973@gmail.com>
2026-02-28 11:47:04 +0500

    Fix urls.py and ensure simplejwt installed

--- hello_project/urls.py
+++ hello_project/urls.py
@@ -7,6 +7,6 @@ from rest_framework_simplejwt.views import TokenObtainPairView
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),  # все эндпоинты будут начинаться с /api/
-    path('api/login/', TokenObtainPairView.as_view(), name='token_obtain_pair')
+    path('api/login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('', TemplateView.as_view(template_name='index.html'))
 ]
