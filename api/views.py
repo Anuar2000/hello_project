e44f9e58d95321a65d00d45af04b4ac212eab5b4
anuar <xudaibergenov1973@gmail.com>
2026-04-04 12:03:10 +0500

    3

--- api/views.py
+++ api/views.py
@@ -2,6 +2,7 @@ from rest_framework import viewsets, generics, status
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import AllowAny
 
 # ИМПОРТ МОДЕЛЕЙ
 # SystemUser — стандартная модель Django для авторизации (с хешированием)
@@ -20,6 +21,7 @@ from .pagination import CustomPageNumberPagination
 
 # --- РЕГИСТРАЦИЯ ---
 class RegisterView(APIView):
+    permission_classes = [AllowAny]
     def post(self, request):
         username = request.data.get('username')
         password = request.data.get('password')
