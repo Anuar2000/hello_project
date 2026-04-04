6bad222b5bbfb75d7c49d564512e4c12ffa56848
anuar <xudaibergenov1973@gmail.com>
2026-04-04 11:28:09 +0500

    2

--- api/admin.py
+++ api/admin.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import User
 from django.contrib.auth.admin import UserAdmin
 from .models import Users, Posts, Comments, Likes, Follows, Media, RefreshTokens
 
-# Регистрируем твои модели
+# Регистрируем твои кастомные таблицы
 admin.site.register(Users)
 admin.site.register(Posts)
 admin.site.register(Comments)
@@ -12,7 +12,7 @@ admin.site.register(Follows)
 admin.site.register(Media)
 admin.site.register(RefreshTokens)
 
-# Перерегистрируем стандартного User, чтобы он точно был виден
+# Выводим системную таблицу пользователей (Auth User)
 try:
     admin.site.unregister(User)
 except admin.sites.NotRegistered:
