f3286c2fc750a485bc7b752d85d89de674c05e82
anuar <xudaibergenov1973@gmail.com>
2026-04-04 05:46:03 +0500

    again create_adimin

--- create_admin.py
+++ create_admin.py
@@ -4,12 +4,12 @@ import django
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'hello_project.settings')
 django.setup()
 
-from api.models import Users # Убедись, что путь к модели верный
+from api.models import Users 
 
 def create_admin():
     username = 'admin'
     email = 'admin@example.com'
-    password = 'твой_очень_сложный_пароль' # ЗАМЕНИ НА СВОЙ
+    password = 'admin' 
     
     if not Users.objects.filter(username=username).exists():
         Users.objects.create_superuser(username=username, email=email, password=password)
