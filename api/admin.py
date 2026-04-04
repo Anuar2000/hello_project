37b725b4c2ec77c14239b470ced7bb7b65a7e2be
anuar <xudaibergenov1973@gmail.com>
2026-04-04 06:39:56 +0500

    выводим все таблицы в админ панеле

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Users, Posts, Comments, Likes, Follows, Media, RefreshTokens
 
-# Register your models here.
+admin.site.register(Users)
+admin.site.register(Posts)
+admin.site.register(Comments)
+admin.site.register(Likes)
+admin.site.register(Follows)
+admin.site.register(Media)
+admin.site.register(RefreshTokens)
